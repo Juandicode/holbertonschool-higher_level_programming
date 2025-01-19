564bde4ef72b9cb409381dbba2ad969f33230485
Juandicode <juandiegomhs@gmail.com>
2025-01-19 20:01:09 +0000

    10

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
 
-    argv = sys.argv[1:]  # Excluye el nombre del script
-    total = sum(int(arg) for arg in argv)  # Convierte los argumento a int y los suma
+    argv = sys.argv[1:]
+    total = sum(int(arg) for arg in argv)
     print(total)

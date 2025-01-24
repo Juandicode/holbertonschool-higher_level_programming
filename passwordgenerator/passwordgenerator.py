d685e37a5702cd2fb20a867f84b706dbf2278d3c
Juandicode <juandiegomhs@gmail.com>
2025-01-24 14:17:02 +0000

    2

---
+++ passwordgenerator/passwordgenerator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+import string
+
+
+myList = ["letras", "simbolos", "numeros"]
+
+    length = int(input("¿Cuántos caracteres debe tener la contrase�"))
+    character_pool = ""
+

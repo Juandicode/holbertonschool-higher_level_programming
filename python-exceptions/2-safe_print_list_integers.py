612b7c8acf96fff763ae7f96dff0848be12f6934
juandicode <juandiegomhs@gmail.com>
2025-01-26 19:52:16 -0300

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             pass
-    print("")  # Añade un salto de línea al final
+    print("")  # Imprime un salto de línea
     return count

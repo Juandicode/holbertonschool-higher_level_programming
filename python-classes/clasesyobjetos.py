36c71ef8a1468cf6eb9b7f98b90e4c702b3c9270
Juandicode <juandiegomhs@mail.com>
2025-01-29 14:06:16 -0300

    square

--- python-classes/clasesyobjetos.py
+++ python-classes/clasesyobjetos.py
@@ -1,12 +1,17 @@
-class Celular():
-    marca = "Samsung"
-    modelo = "S23"
-    camara = "48MP"
+class Celular:
+   def __init__(self, marca, modelo, camara):
+    self.marca = marca
+    self.modelo = modelo
+    self.camara = camara
 
-celular1 = Celular()
-celular2 = Celular()
-celular3 = Celular()
-celular4 = Celular()
+    def llamar(self):
+        print(f'Estas haciendo un llamado desde un: {self.modelo}')
+
+    def cortar(self):
+        print(f'Cortaste la llamada desde tu: {self.modelo}')
+
+celular1 = Celular("Samsung", "S23","48MP")
+celular2 = Celular("Apple", "Iphone 15 Pro","96MP")
 
 
-print(celular3.marca)
+celular1.llamar()

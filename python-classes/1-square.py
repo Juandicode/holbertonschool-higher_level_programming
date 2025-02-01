4eccacbf2ecd783c28a4c412e7ee75821cd73862
juandicode <juandiegomhs@gmail.com>
2025-01-31 23:41:34 -0300

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 class Square:
     """Define a square."""
+    
     def __init__(self, size):
         """Initialize a square with a given size.
         Args:
             size: The size of the square.
         """
-       self.__size = size
+        self.__size = size

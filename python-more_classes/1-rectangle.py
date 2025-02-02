097eb8a6bb25eae9e06a11f571a03b749975e5dc
juandicode <juandiegomhs@gmail.com>
2025-02-02 14:52:47 -0300

    define an empyt rectangle class

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a Rectangle class with private attributes width and height."""
 
+
 class Rectangle:
     """Represent a rectangle."""
 
@@ -39,3 +40,4 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+        

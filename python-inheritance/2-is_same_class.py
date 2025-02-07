840e518bce49c33a00125c12a879e5112e7d980f
juandicode <juandiegomhs@gmail.com>
2025-02-06 21:51:22 -0300

    funcion que devuelve available attributes and methods of an object

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-is_same_class = __import__('2-is_same_class').is_same_class
 
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
+import importlib.util
+import sys
+
+def is_same_class(obj, a_class):
+    """Devuelve True si obj es exactamente una instancia de a_class, de lo contrario False"""
+    return type(obj) is a_class
+
+# Esta parte importaría el archivo dinámicamente
+spec = importlib.util.spec_from_file_location("2-is_same_class", "/home/student_jail/student_repo/python-inheritance/2-is_same_class.py")
+module = importlib.util.module_from_spec(spec)
+sys.modules["2-is_same_class"] = module
+spec.loader.exec_module(module)
+
+# Luego puedes usar is_same_class como antes

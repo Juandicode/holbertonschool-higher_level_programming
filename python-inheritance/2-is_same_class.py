b7aa0aca09501deb563b9b9e2419af0972bae549
juandicode <juandiegomhs@gmail.com>
2025-02-07 19:45:42 -0300

    2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 
-import importlib.util
-import sys
-
 def is_same_class(obj, a_class):
-    """Devuelve True si obj es exactamente una instancia de a_class, de lo contrario False"""
+    """
+    Returns True if obj is exactly an instance of a_class, otherwise False.
+    """
     return type(obj) is a_class
-
-# Esta parte importaría el archivo dinámicamente
-spec = importlib.util.spec_from_file_location("2-is_same_class", "/home/student_jail/student_repo/python-inheritance/2-is_same_class.py")
-module = importlib.util.module_from_spec(spec)
-sys.modules["2-is_same_class"] = module
-spec.loader.exec_module(module)
-
-# Luego puedes usar is_same_class como antes

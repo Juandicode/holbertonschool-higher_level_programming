8f1324f96b4df073fce9b19c56bc87861c49dd99
juandicode <juandiegomhs@gmail.com>
2025-02-06 21:13:32 -0300

    funcion que devuelve available attributes and methods of an object

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,6 @@
 class MyList(list):
     """Clase que hereda de list y proporciona un método para imprimir la lista ordenada."""
     
-    
-	def print_sorted(self):
+    def print_sorted(self):
         """Imprime la lista en orden ascendente."""
         print(sorted(self))

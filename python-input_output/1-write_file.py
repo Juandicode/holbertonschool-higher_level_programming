6fb82e276c744edf35b3d1a9736f6e23a48b1f84
Juandicode <juandiegomhs@mail.com>
2025-02-11 15:05:28 -0300

    my file.txt

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Este módulo contiene la función write_file que escribe un texto en un archivo y devuelve el número de caracteres escritos."""
+"""Este módulo contiene la función write_file que escribe
+un texto en un archivo y devuelve el número de caracteres escritos."""
 
 
 def write_file(filename="", text=""):
-    """Escribe un texto en un archivo y devuelve el número de caracteres escritos."""
-    
-    # Abrimos el archivo en modo de escritura ('w'), lo que crea el archivo si no existe o lo sobrescribe si ya existe
+    """Escribe un texto en un archivo y
+    devuelve el número de caracteres escritos."""
+
+    """ Abrimos el archivo en modo de escritura ('w'), 
+    lo que crea el archivo si no existe o lo sobrescribe si ya existe"""
     with open(filename, "w", encoding="utf-8") as file:
-        chars_written = file.write(text)  # Escribe el texto en el archivo y obtiene el número de caracteres escritos
+        chars_written = file.write(text)  # Escribe el texto en el archivo y obtiene el núm de caract scritos
     return chars_written  # Devuelve el número de caracteres escritos

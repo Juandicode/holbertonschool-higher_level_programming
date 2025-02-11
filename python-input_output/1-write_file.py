602b6f69862268d051d3c5ffb91e6786be8cfc8e
Juandicode <juandiegomhs@mail.com>
2025-02-11 14:48:00 -0300

    my file.txt

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,7 +7,7 @@ def write_file(filename="", text=""):
 
 
 """ Usamos 'with' para abrir el archivo
-    en modo escribir (write) ('w') con codificación UTF-8"""
+    en modo escribir (write) ('w') con codificación UTF-8 """
     with open(filename, "w", encoding="utf-8") as file:
 
         chars_written = file.write(text)  # Escribe el texto en el archivo y obtiene el número de caracteres escritos

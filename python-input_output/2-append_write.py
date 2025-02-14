ce73fee797f01e5bd0e963f673d5f034da754db8
Juandicode <146032078+Juandicode@users.noreply.github.com>
2025-02-14 09:47:52 -0300

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,4 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 
 def append_write(filename="", text=""):
     """Appends a string at the end of a text file (UTF8) and returns the number of characters added."""

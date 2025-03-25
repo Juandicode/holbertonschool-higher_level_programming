c44e2f3e2aae1fe0fc2f326c68384973d05f8971
juandicode <juandiegomhs@gmail.com>
2025-03-25 19:55:16 -0300

    mi pocoyo

--- learning/pythonclases/polimorfismo.py
+++ learning/pythonclases/polimorfismo.py
@@ -5,3 +5,5 @@ Create a class `Shape` with a method `area()` that raises a `NotImplementedError
 Create two subclasses `Circle` and `Rectangle` that implement the `area()` method.
 Write a function that takes a `Shape` object and prints its area. Test it with instances of `Circle` and `Rectangle`.
 ---"""
+class Shape:
+    __init__(self)

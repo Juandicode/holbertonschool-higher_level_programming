6431576cc4761151eb0e5d2590e258e064299936
Juandicode <juandiegomhs@mail.com>
2025-04-01 12:58:11 -0300

    poniendo en api los user amenity y reviewsy place

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
+"""
+python script that lists all states from the database hbtn_0e_0_usa
+"""
 
-""" Enumera todos los estados de la base de datos HBTN_0E_0_USA """
+import MySQLdb
+from sys import argv
 
-if __name__ == '__main__':
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    rows = cur.fetchall()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
+    cursor.close()
+    db.close()

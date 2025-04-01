3da5cd09d14906d826dd9663f7a1b4e4d48a1003
Juandicode <juandiegomhs@mail.com>
2025-04-01 15:54:06 -0300

    en la misma database cambio lo que ejecuta el cursor par que me muestreloos datos empezando con n en orden

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,10 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(
-        query, (argv[4],)
-    )  # Se pasa como tupla para evitar inyección SQL
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4])
+    cursor.execute(query)  # Se pasa como tupla para evitar inyección SQL
     rows = cursor.fetchall()
     for row in rows:
         print(row)

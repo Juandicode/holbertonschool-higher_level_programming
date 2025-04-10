88a2dbb37a7b41abc657eb4e92e7115394ef7ce8
Juandicode <juandiegomhs@mail.com>
2025-04-10 13:23:30 -0300

    añadiendo un atributo publico para poder cambiar el simbolo del rectangulo por cualquiera que se pase en el main

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -76,14 +76,12 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-
-
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-    
-        # If both areas are equal, we can return either rect_1 or rect_2 (same object)
+
+        # If las dos areas son iguales, podemos retornar cualquiera
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2

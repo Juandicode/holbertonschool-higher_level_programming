7e8622efe63fb767c5500e3a7fde429619c40979
Juandicode <juandiegomhs@mail.com>
2025-05-06 12:11:44 -0300

    print a string 3 times without a space in between  and then followed by its first 9 characters

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,4 +1,3 @@
-Complete this source code in order to print 3 times a string stored in the variable str, followed by its first 9 characters.
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{str} {str} {str}/n{str[:9]}")
+print(f"{str}{str}{str}\n{str[:9]}")

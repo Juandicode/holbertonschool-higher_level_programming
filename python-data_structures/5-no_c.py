0211142f1f92ebdd300d36b6853571568f8e200c
Juandicode <juandiegomhs@mail.com>
2025-05-14 10:43:30 -0300

    object oriented prgramming with python exercise

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def no_c(my_string):
+    # crea una nueva cadena sin las letras c y C
+    new_string = "" # inicializa una nueva cadena vacía 
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string # devuelve la nueva cadena sin las letras c y C

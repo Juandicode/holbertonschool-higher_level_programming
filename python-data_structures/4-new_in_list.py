f877266be67f16ff06c7febf16792729c6e6425f
Juandicode <juandiegomhs@mail.com>
2025-05-14 10:36:14 -0300

    remplazarr un elemento de una lista sin modificar la original

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    # si el indice es negatio o mayor al tamaño de la lista
     if idx < 0 or idx >= len(my_list):
-        return my_list[:]
+        return my_list[:] # devuelve una copia de la lista
 
-    new_list = my_list[:]
-    new_list[idx] = element
+    new_list = my_list[:]  # esto cre una copia de la lista orgiinal pa no modificarla
+    new_list[idx] = element # reemplaza el elemento en la copia de la lista
     return new_list

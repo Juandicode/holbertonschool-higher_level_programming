99931d3675cb3682ce31f0db909a374911579658
Juandicode <juandiegomhs@mail.com>
2025-05-14 13:04:45 -0300

    funcion que printea una matriz de integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        formateado_row = []  # lista vacía para almacenar los números formateados
+        formateado_row = []  # lista vacía
         for i in row:  # itera sobre cada elemento de la fila
-            numero_formateado = "{:d}".format(i)  # formatea el número como cadena
-            formateado_row.append(numero_formateado)  # agrega el número formateado a la lista
+            numero_formateado = "{:d}".format(i)  # formatea el número
+            formateado_row.append(numero_formateado)  # agrega el número formateado
         # une los números formateados con un espacio y los imprime
         # el join une los elementos de la lista con un espacio entre ellos
         print(" ".join(formateado_row))

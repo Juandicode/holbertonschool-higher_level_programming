156e00fec1bb8389d10b45e9b9cef185bbdfa1f0
Juandicode <juandiegomhs@mail.com>
2025-05-14 12:55:58 -0300

    object oriented prgramming with python exercise

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join("{:d}".format(i) for i in row))
+        for i in row:
+            formatted_number = "{:d}".format(i)
+            formatted_row.append(formatted_number)
+        print(" ".join(formatted_row))
+
+
+# "{:d}".format(i) esto convierte cada numero enter en una cadena con formato decimal ej: 1 -> "1" , 2 -> "2"

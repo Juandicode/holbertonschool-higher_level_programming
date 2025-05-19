4a3d4631a5f6c4ab6194def65609c3331a74fe76
Juandicode <juandiegomhs@mail.com>
2025-05-19 12:41:09 -0300

    creae a class square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""defin0 la clase cuadrado """
 class Square:
+    """ la classe definida(square) """
     def __init__(self, size):
+        """inicializo el cuadrado con un tamaño  """
         self.__size = size

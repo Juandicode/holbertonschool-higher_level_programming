22e84b3bf00cec69cc1cde54078ae09ef68f17e9
Juandicode <juandiegomhs@mail.com>
2025-05-20 14:48:26 -0300

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""modulo q define la clase cuaudrado."""
+
+
+class Square:
+    """la class square define un cuadrado."""
+
+    def __init__(self, size=0):
+        """inicializo un cuadrado con un tamaño.
+
+        Args:
+            size: el tamaño de la clase."""
+        if not isinstance(size, int):
+            raise TypeError("el tamaño tiene que ser un integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size

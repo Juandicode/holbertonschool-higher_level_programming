34f5866b9a2761bbe2818b6a13299aa4fb9ab87a
Juandicode <juandiegomhs@mail.com>
2025-05-21 09:58:59 -0300

    getter ad setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,13 +10,23 @@ class Square:
 
         Args:
             size: el tamaño de la clase."""
-        if not isinstance(size, int):
+        self.size = size
+        
+        
+    @property
+    def size(self):
+        """getter del tamaño."""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """Setter para establecer el tamaño del cuadrado con validaciones."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """ devuelve el area del cuadrado."""
         return self.__size ** 2
-

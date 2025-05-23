80ca715e51a6627266f94c67ea7b2139db737353
Juandicode <juandiegomhs@mail.com>
2025-05-23 12:24:34 -0300

    clase rectangulo

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class with area and perimeter methods."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+

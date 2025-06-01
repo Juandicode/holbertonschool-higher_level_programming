969a0bf037e64ee7f936d55b4d582410d9937065
juandicode <juandiegomhs@gmail.com>
2025-06-01 19:28:04 -0300

    mylist hereda list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Muodulo pa clase mylist.
+"""
+
+class MyList(list):
+    """
+   clase mylist hereda d list.
+    """
+
+    def print_sorted(self):
+        """
+        printea la lista en orden ascendente.
+        """
+        print(sorted(self))

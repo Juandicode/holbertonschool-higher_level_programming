937d38378ddcbf48cd3facab587fab81c071d554
juandicode <juandiegomhs@gmail.com>
2025-06-01 20:16:14 -0300

    escribo una funcion que retorne positivo si el obketo es una clasde

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a function that returns
+True if the object is an
+instance of a class that inherited (directly or
+indirectly) from the specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if obj is an instance of a subclass of a_class"""
+    return isinstance(obj, a_class) and type(obj) is not a_class

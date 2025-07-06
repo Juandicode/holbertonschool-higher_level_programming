c85fa80ae2f8d7b40bfad21e69dfa093b6c370ec
Juandicode <juandiegomhs@mail.com>
2025-07-06 19:34:02 -0300

    print objet ith the name passed as an argmunet

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,6 +4,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     """City class linked to MySQL table 'cities'"""
     __tablename__ = 'cities'

ed79b0ace68311f3211eb5db578d644f31c38477
Juandicode <juandiegomhs@mail.com>
2025-07-06 15:31:24 -0300

    chancging 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 '''
-makinng a script that
-gets information from a database
+Making a script that gets information from a database
 '''
 import MySQLdb
 from sys import argv
 
-
-if __name__ == "__name__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
     for row in rows:
-        print(cur.fetchone())
+        print(row)
     cur.close()
     db.close()
